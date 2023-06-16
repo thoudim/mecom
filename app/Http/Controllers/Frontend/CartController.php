9fb81d5e3d80a00321412cd073b560463fd0e040
fiya1991 <afiyahmat@gmail.com>
2023-06-17 00:06:38 +0800

    Finished section 40,41,42

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Coupon;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Support\Facades\Session;
+use Carbon\Carbon;
 
 class CartController extends Controller
 {
@@ -149,6 +151,19 @@ class CartController extends Controller
         $row = Cart::get($rowId);
         Cart::update($rowId, $row->qty -1);
 
+        if(Session::has('coupon')){
+
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name', $coupon_name)->first();
+
+            Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name,
+                'coupon_discount' => $coupon->coupon_discount,
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100),
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100),
+            ]);
+        }
+
         return response()->json('Decrement');
 
     }// End Method
@@ -158,7 +173,68 @@ class CartController extends Controller
         $row = Cart::get($rowId);
         Cart::update($rowId, $row->qty +1);
 
+        if(Session::has('coupon')){
+
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name', $coupon_name)->first();
+
+            Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name,
+                'coupon_discount' => $coupon->coupon_discount,
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100),
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100),
+            ]);
+        }
+
         return response()->json('Increment');
+
+    }// End Method
+
+    public function CouponApply(Request $request)
+    {
+        $coupon = Coupon::where('coupon_name', $request->coupon_name)->where('coupon_validity', '>=', Carbon::now()->format('Y-m-d'))->first();
+
+        if($coupon){
+            Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name,
+                'coupon_discount' => $coupon->coupon_discount,
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100),
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100),
+            ]);
+
+            return response()->json(array(
+                'validity' => true,
+                'success' => 'Coupon Applied Successfully'
+            ));
+        }else{
+            return response()->json(['error' => 'Invalid Coupon']);
+        }
+
+    }// End Method
+
+    public function CouponCalculation()
+    {
+        
+        if(Session::has('coupon')){
+            return response()->json(array(
+                'subtotal' => Cart::total(),
+                'coupon_name' => session()->get('coupon')['coupon_name'],
+                'coupon_discount' => session()->get('coupon')['coupon_discount'],
+                'discount_amount' => session()->get('coupon')['discount_amount'],
+                'total_amount' => session()->get('coupon')['total_amount'],
+            ));
+        }else{
+            return response()->json(array(
+                'total' => Cart::total(),
+            ));
+        }
+
+    }// End Method
+
+    public function CouponRemove()
+    {
+        Session::forget('coupon');
+        return response()->json(['success' => 'Coupon Removed Successfully']);
 
     }// End Method
 }
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\User\WishlistController;
 use App\Http\Controllers\User\CompareController;
+use App\Http\Controllers\Backend\CouponController;
+use App\Http\Controllers\Backend\ShippingAreaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -178,7 +180,53 @@ Route::middleware(['auth','role:admin'])->group(function() {
         Route::get('/delete/banner/{id}' , 'DeleteBanner')->name('delete.banner');
     });
 
-});
+    // Coupon All Route 
+    Route::controller(CouponController::class)->group(function(){
+        Route::get('/all/coupon' , 'AllCoupon')->name('all.coupon');
+        Route::get('/add/coupon' , 'AddCoupon')->name('add.coupon');
+        Route::post('/store/coupon' , 'StoreCoupon')->name('store.coupon');
+        Route::get('/edit/coupon/{id}' , 'EditCoupon')->name('edit.coupon');
+        Route::post('/update/coupon' , 'UpdateCoupon')->name('update.coupon');
+        Route::get('/delete/coupon/{id}' , 'DeleteCoupon')->name('delete.coupon');
+
+    });
+
+    // Shipping Division All Route 
+    Route::controller(ShippingAreaController::class)->group(function(){
+        Route::get('/all/division' , 'AllDivision')->name('all.division');
+        Route::get('/add/division' , 'AddDivision')->name('add.division');
+        Route::post('/store/division' , 'StoreDivision')->name('store.division');
+        Route::get('/edit/division/{id}' , 'EditDivision')->name('edit.division');
+        Route::post('/update/division' , 'UpdateDivision')->name('update.division');
+        Route::get('/delete/division/{id}' , 'DeleteDivision')->name('delete.division');
+
+    });
+
+    // Shipping District All Route 
+    Route::controller(ShippingAreaController::class)->group(function(){
+        Route::get('/all/district' , 'AllDistrict')->name('all.district');
+        Route::get('/add/district' , 'AddDistrict')->name('add.district');
+        Route::post('/store/district' , 'StoreDistrict')->name('store.district');
+        Route::get('/edit/district/{id}' , 'EditDistrict')->name('edit.district');
+        Route::post('/update/district' , 'UpdateDistrict')->name('update.district');
+        Route::get('/delete/district/{id}' , 'DeleteDistrict')->name('delete.district');
+
+    });
+
+    // Shipping State All Route 
+    Route::controller(ShippingAreaController::class)->group(function(){
+        Route::get('/all/state' , 'AllState')->name('all.state');
+        Route::get('/add/state' , 'AddState')->name('add.state');
+        Route::post('/store/state' , 'StoreState')->name('store.state');
+        Route::get('/edit/state/{id}' , 'EditState')->name('edit.state');
+        Route::post('/update/state' , 'UpdateState')->name('update.state');
+        Route::get('/delete/state/{id}' , 'DeleteState')->name('delete.state');
+
+        Route::get('/district/ajax/{division_id}' , 'GetDistrict');
+
+    }); 
+
+}); // Admin End Middleware
 
 // Frontend Product Details All Route
 Route::get('/product/details/{id}/{slug}', [IndexController::class, 'ProductDetails']);
@@ -200,6 +248,11 @@ Route::post('/dcart/data/store/{id}', [CartController::class, 'AddToCartDetails'
 Route::post('/add-to-wishlist/{product_id}', [WishlistController::class, 'AddToWishlist']);
 // Add to Compare
 Route::post('/add-to-compare/{product_id}', [CompareController::class, 'AddToCompare']);
+
+// Frontend Coupon Option
+Route::post('/coupon-apply', [CartController::class, 'CouponApply']);
+Route::get('/coupon-calculation', [CartController::class, 'CouponCalculation']);
+Route::get('/coupon-remove', [CartController::class, 'CouponRemove']);
 
 /// User All Route
 Route::middleware(['auth','role:user'])->group(function(){
